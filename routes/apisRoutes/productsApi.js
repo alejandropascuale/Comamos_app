b957813358b3dc15caa371ca6291fb1bcc191870
alejandropascuale <apascuale@outlook.com>
2021-09-15 14:10:19 -0300

    create a count item in cart and add coding to identificate the product to reserve

--- routes/apisRoutes/productsApi.js
+++ routes/apisRoutes/productsApi.js
@@ -4,6 +4,6 @@ const router = express.Router();
 const productApiController = require('../../controllers/apisControllers/productApiController');
 
 router.get('/products', productApiController.productsList);
-router.get('/products/:idPlato',  productApiController.selectProduct);
+router.get('/product/:idPlato',  productApiController.selectProduct);
 
 module.exports = router;
